4e35d416babf4596ee795e70a489e381e30c8349
Sjors Provoost <sjors@sprovoost.nl>
2014-06-26 14:15:41 +0200

    Turned into class and moved functions to super.

--- CoinKey.swift
+++ CoinKey.swift
@@ -5,24 +5,10 @@
 //  Created by Sjors Provoost on 23-06-14.
 //
 
-import Foundation
+import CryptoCoin
 
-struct CoinKey {
-    let privateKey: UInt256
-    
-    let publicAddress : String
-    
-    var privateKeyHexString: String {
-        return privateKey.toHexString
+class CoinKey : ECKey {
+    var publicAddress : String {
+        return "Wrong"
     }
-    
-    init(privateKey: UInt256) {
-        self.privateKey = privateKey
-        self.publicAddress = "Wrong"
-    }
-    
-    static func createRandom () -> CoinKey {
-        return CoinKey(privateKey: UInt256(mostSignificantOf8UInt32First: [arc4random_uniform(UInt32.max), arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max),arc4random_uniform(UInt32.max)]))
-    }
-    
 }
--- CoinKeyTests.swift
+++ CoinKeyTests.swift
@@ -19,28 +19,7 @@ class CoinKeyTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testInitWithPrivateKey() {
-        let coinKey = CoinKey(privateKey: 1)
         
-        XCTAssertEqual(coinKey.privateKey , 1, "Private key correct");
-    }
-    
-    func testCreateRandom() {
-        let coinKey1 = CoinKey.createRandom();
-        let coinKey2 = CoinKey.createRandom();
-        
-        XCTAssertNotEqual(coinKey1.privateKey, coinKey2.privateKey, "Two random private keys can't be equal");
-        
-    }
-    
-    func testPrivateKeyHexString() {
-        let coinKey = CoinKey(privateKey: 0x1F)
-        
-        XCTAssertEqual(coinKey.privateKeyHexString , "1F", "Hex string");
-
-    }
-    
     func testPublicAddress() {
         let coinKey = CoinKey(privateKey: 1); // 1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd
         
